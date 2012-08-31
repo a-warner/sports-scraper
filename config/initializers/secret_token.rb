bc1df065f807c2203641366c11c825e65c46dd1d
Andrew Warner <wwarner.andrew@gmail.com>
2012-08-31 15:12:50 -0400

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsScraper::Application.config.secret_token = '260cdda53743e3cb57c49e1b509d2fc12e4ae1425110c83a872d141c68bd4b26dd15330f04a40ad68160cca51c3ed5085c6563b0cab0098bcecf3c0dd6555b37'
