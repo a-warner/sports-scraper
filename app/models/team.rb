3d0b710d754d510518776a9718852e0ee22490d5
Andrew Warner <wwarner.andrew@gmail.com>
2012-08-31 16:55:39 -0400

    Add depth chart uri generation

--- app/models/team.rb
+++ app/models/team.rb
@@ -2,4 +2,10 @@ class Team < ActiveRecord::Base
   attr_accessible :name, :url
   validates :name, :url, presence: true
   validates :name, uniqueness: { scope: :name }
+  has_many :players
+  has_many :player_depths
+
+  def depth_chart_uri
+    url.gsub("team","team/depth")
+  end
 end
