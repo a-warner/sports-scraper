42b81a1393b680ba4132573d06129d1321cc1419
Andrew Warner <wwarner.andrew@gmail.com>
2012-09-01 19:00:53 -0400

    Add position filtering for player depth

--- app/models/player_depth.rb
+++ app/models/player_depth.rb
@@ -5,4 +5,15 @@ class PlayerDepth < ActiveRecord::Base
 
   attr_accessible :depth
   validates :depth, presence: true, uniqueness: { scope: [:team_id, :position_id] }
+
+  default_scope order(:depth)
+
+  scope :at_position, ->(position) do
+    if position.is_a? String
+      position_id = Position.find_by_code(position)
+    else
+      position_id = position.id
+    end
+    where(position_id: position_id)
+  end
 end
