bdd6ce229bc3b6be4699d214f08fbaab34132615
Andrew Warner <wwarner.andrew@gmail.com>
2013-08-29 10:12:18 -0400

    Redirect to exact matches if found

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,11 +4,12 @@ class SearchController < ApplicationController
     Player.for_query(query) + Team.for_query(query)
   end
 
-  def index
-
-  end
-
   def new
+    if results.one? && !request.xhr?
+      redirect_to polymorphic_path(results.first), status: :found
+      return
+    end
+
     respond_to do |format|
       format.js { render partial: 'results', locals: { results: results }, content_type: 'text/html' }
       format.html
