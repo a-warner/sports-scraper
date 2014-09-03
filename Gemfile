97b2094bbd02be5f2f2e5960af819f1f3121b7ae
Andrew Warner <wwarner.andrew@gmail.com>
2014-09-02 22:04:17 -0400

    Cache autocomplete data

--- Gemfile
+++ Gemfile
@@ -2,21 +2,21 @@ source 'https://rubygems.org'
 ruby '1.9.3'
 
 gem 'active_model_serializers'
+gem 'dalli'
 gem 'decent_exposure'
 gem 'haml'
 gem 'jquery-rails'
+gem 'memcache-client'
 gem 'nokogiri'
 gem 'pry'
 gem 'rails', '3.2.7'
 gem 'sqlite3'
 gem 'thin'
 
-group :assets do
-  gem 'sass'
-  gem 'sass-rails',   '~> 3.2.3'
-  gem 'coffee-rails', '~> 3.2.1'
-  gem 'uglifier', '>= 1.0.3'
-end
+gem 'sass'
+gem 'sass-rails',   '~> 3.2.3'
+gem 'coffee-rails', '~> 3.2.1'
+gem 'uglifier', '>= 1.0.3'
 
 group :test, :development do
   gem 'fabrication'
--- app/models/autocomplete.rb
+++ app/models/autocomplete.rb
@@ -1,9 +1,19 @@
 module Autocomplete
   include Rails.application.routes.url_helpers
   extend self
+  delegate :expand_cache_key, to: 'ActiveSupport::Cache'
+  delegate :perform_caching, to: 'ActionController::Base'
 
   def data
-    paths = Team.all.map { |t| [t.name, team_path(t)] } + Player.all.map { |p| [p.name, player_path(p)] }
-    paths.each_with_object({}) { |(n, p), h| h[n] = p }
+    cache [Team.order(updated_at: :desc).last, Player.order(updated_at: :desc).last] do
+      paths = Team.all.map { |t| [t.name, team_path(t)] } + Player.all.map { |p| [p.name, player_path(p)] }
+      paths.each_with_object({}) { |(n, p), h| h[n] = p }
+    end
+  end
+
+  def cache(key, &block)
+    return yield unless perform_caching
+
+    Rails.cache.fetch(expand_cache_key(key), &block)
   end
 end
